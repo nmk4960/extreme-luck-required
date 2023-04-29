bd3afd23ce97bd48ad81d8377517821c11bddc82
nmk4960 <132114171+nmk4960@users.noreply.github.com>
2023-04-28 19:57:04 -1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let trys = 0;
+function luck() {
+	let res;
+	res = dice();
+	if(res === 50) {
+		document.querySelector('button').disabled = true;
+		document.querySelector('button').innerText = "Refresh the page.";
+		res = "yes!";
+		var audio = document.getElementById("myAudio");
+		audio.play();
+	}
+	else res = "No!";
+	trys++;
+	document.getElementById("res").innerHTML = res;
+	document.getElementById("trys").innerHTML = trys;
+}
+
+function dice() {
+	const random_number1 = Math.floor(Math.random() * 100);
+	const random_number2 = Math.floor(Math.random() * 100);
+	let res = 0, i = 0;;
+
+	while(i < 50) {
+		if(random_number1 == random_number2) res++;
+		console.log(res);
+		i++;
+	}
+	return res;
+}
